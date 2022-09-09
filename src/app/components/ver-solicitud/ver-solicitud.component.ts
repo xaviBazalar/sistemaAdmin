da0f1c37f0819b2940980404d5a9bb3494144abd
Xavi Bazalar <xavibazalar@MacBook-Pro-de-Xavi.local>
2022-09-09 15:15:35 -0500

    fix

--- src/app/components/ver-solicitud/ver-solicitud.component.ts
+++ src/app/components/ver-solicitud/ver-solicitud.component.ts
@@ -159,7 +159,7 @@ export class VerSolicitudComponent implements OnInit {
 
 
   getTareaDocumentosEntrada(tarea:string) {
-    this.tareaDocumentosService.getTareaDocumentosEntrada(tarea,this.solicitud.contrato).subscribe((data:any)=>{
+    this.tareaDocumentosService.getTareaDocumentosEntrada(tarea,this.solicitud.contrato._id).subscribe((data:any)=>{
       this.listaTareaDocumentos=data.tarea_documentos_entrada;
     })
   }
@@ -179,7 +179,7 @@ export class VerSolicitudComponent implements OnInit {
   }
 
   getTareaDocumentosSalida(tarea:string) {
-    this.tareaDocumentosSalidaService.getTareaDocumentosSalida(tarea,this.solicitud.contrato).subscribe((data:any)=>{
+    this.tareaDocumentosSalidaService.getTareaDocumentosSalida(tarea,this.solicitud.contrato._id).subscribe((data:any)=>{
       this.listaTareaDocumentosSalida=data.tarea_documentos_salida;
     })
   }
