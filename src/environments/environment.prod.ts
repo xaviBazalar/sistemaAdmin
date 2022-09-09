8e4139f59804d1361dadb45ad398bfbbd1b451c1
Xavi Bazalar <xavibazalar@MacBook-Pro-de-Xavi.local>
2022-09-08 19:21:27 -0500

    update fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:"http://54.227.87.129:8080/api",
-  baseUrlGet:"http://54.227.87.129:8080"
+  baseUrl:"http://34.172.89.68:8080/api",
+  baseUrlGet:"http://34.172.89.68:8080"
 };
