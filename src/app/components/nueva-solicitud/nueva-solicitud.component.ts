c6b3fd042415efa52f7d464bdba3784e0a3dfbf5
Xavi Bazalar <xavibazalar@MacBook-Pro-de-Xavi.local>
2022-11-18 09:44:10 -0500

    fix

--- src/app/components/nueva-solicitud/nueva-solicitud.component.ts
+++ src/app/components/nueva-solicitud/nueva-solicitud.component.ts
@@ -175,7 +175,7 @@ export class NuevaSolicitudComponent implements OnInit {
     let idContrato:string=""
     for(let contrato of this.listaContratos){
       console.log(contrato)
-      let nTemp=contrato.contrato.contrato+"-"+contrato.contrato.contradoid+"-"+contrato.contrato.adc.nombre
+      let nTemp=contrato.contrato.contrato+"-"+contrato.contrato.contradoid//+"-"+contrato.contrato.adc.nombre
       if((nTemp.trim())==contratoTxt.trim()){
         idContrato=contrato.contrato._id
         this.contratoTemp=contrato.contrato._id
