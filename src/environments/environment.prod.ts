960673bbb4b345840e3179eefa6fe618e3d0d005
Xavi Bazalar <xavibazalar@MacBook-Pro-de-Xavi.local>
2022-12-07 17:31:24 -0500

    last changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  baseUrl:"http://34.172.89.68:8080/api",
-  baseUrlGet:"http://34.172.89.68:8080",
+  baseUrl:"http://35.208.114.201:8080/api",
+  baseUrlGet:"http://35.208.114.201:8080",
   baseUrlApiEmail:"https://mailingperu.intico.com.pe/mailing/v1/demo/EnviarMailing",
   apiKey:"62940aa827dd600bfe1d65511282c1b5c577451c2b9002bfe326e83627fc0673",
   userApiEmail:"Mining",
-  baseUrlApp:"http://34.172.89.68/"
+  baseUrlApp:"http://35.208.114.201/"
 };
 
 /*
